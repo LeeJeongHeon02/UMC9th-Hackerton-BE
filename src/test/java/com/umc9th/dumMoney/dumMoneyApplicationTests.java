86e78aad1ddf9ac5814c2fa5af170887cfcd53cb
Yoonssu <orvosdoktor21@gmail.com>
2026-01-10 16:52:54 +0900

    Fix : 프로젝트명 수정

--- src/test/java/com/umc9th/dumMoney/dumMoneyApplicationTests.java
+++ src/test/java/com/umc9th/dumMoney/dumMoneyApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BizscanApplicationTests {
+class dumMoneyApplicationTests {
 
     @Test
     void contextLoads() {
